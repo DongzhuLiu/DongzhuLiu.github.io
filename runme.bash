625cac59c7a1a119c02fcb3571a3bfc54e1285e9
DongzhuLiu <80288539+DongzhuLiu@users.noreply.github.com>
2023-10-09 21:49:18 +0100

    Add files via upload

--- runme.bash
+++ runme.bash
@@ -1,5 +1,6 @@
 python jemdoc.py home.jemdoc
 python jemdoc.py cv.jemdoc
+python jemdoc.py team.jemdoc
 python jemdoc.py publications.jemdoc
 python jemdoc.py teaching.jemdoc
 python jemdoc.py services.jemdoc
